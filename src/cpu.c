a7e6ea5ddb125b383789b3b9046adddccc02b8f7
Jason Long <jasonlongball@gmail.com>
2022-05-22 20:55:20 -0400

    CPU: Print out Process Status properly

--- src/cpu.c
+++ src/cpu.c
@@ -28,5 +28,13 @@ void set_processor_status(cpu_t *cpu, enum ps_flag flag, bool val)
 // TODO: This should return a string not print
 void cpu_registers_to_string(const cpu_t *cpu)
 {
-  dbgln("PC: 0x%04X PS: 0x%02X SP: 0x%02X A: 0x%02X X: 0x%02X Y: 0x%02X", cpu->pc, cpu->ps, cpu->sp, cpu->a, cpu->x, cpu->y);
+  dbgln("PC: 0x%04X SP: 0x%02X A: 0x%02X X: 0x%02X Y: 0x%02X", cpu->pc, cpu->sp, cpu->a, cpu->x, cpu->y);
+  dbgln("N: %d V: %d B: %d D: %d I: %d Z: %d C: %d",
+        get_processor_status(cpu, PS_NEGATIVE),
+        get_processor_status(cpu, PS_OVERFLOW),
+        get_processor_status(cpu, PS_BREAK),
+        get_processor_status(cpu, PS_DECIMAL),
+        get_processor_status(cpu, PS_INTERRUPT),
+        get_processor_status(cpu, PS_ZERO),
+        get_processor_status(cpu, PS_CARRY));
 }
